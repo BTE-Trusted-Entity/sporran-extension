ba8d5732abe2b464846b0baf57ded40efe4ecb81
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2021-03-09 13:20:10 +0100

    removed changes not involving Kilt SDK

---
+++ src/screens/CreateAccount/CreateAccount.tsx
@@ -0,0 +1,24 @@
+import { FC, useEffect, useState } from 'react';
+import { Identity, init } from '@kiltprotocol/core';
+
+export const CreateAccount: FC = () => {
+  const [mnemonic, setMnemonic] = useState<string>();
+  const [address, setAddress] = useState<string>();
+  useEffect(() => {
+    (async (): Promise<void> => {
+      await init({ address: 'wss://full-nodes.kilt.io:9944' });
+      const mnemonic = Identity.generateMnemonic();
+      setMnemonic(mnemonic);
+      console.log('Mnemonic: ', mnemonic);
+      const identity = Identity.buildFromMnemonic(mnemonic);
+      setAddress(identity.address);
+      console.log('Claimer identity: ', identity);
+    })();
+  }, []);
+  return (
+    <>
+      <h1>{mnemonic}</h1>
+      <h1>{address}</h1>
+    </>
+  );
+};
