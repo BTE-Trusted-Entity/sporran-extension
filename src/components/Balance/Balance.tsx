a7ae917304e016caddf4cc385c8a3f8e6e2a163f
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2021-03-11 11:49:53 +0100

    removed fixed decimal places to allow for more precision

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -6,7 +6,7 @@ import {
 import BN from 'bn.js';
 import { ClipLoader } from 'react-spinners';
 
-const KILT_FEMTO_COIN = 1000000000000000;
+const KILT_FEMTO_COIN = 1e15; // or 1_000_000_000_000_000
 
 interface BalanceProps {
   address: string;
@@ -16,10 +16,11 @@ export function Balance({ address }: BalanceProps): JSX.Element {
   const [balance, setBalance] = useState('');
 
   function asKiltCoins(balance: BN): string {
-    return balance.div(new BN(KILT_FEMTO_COIN)).toNumber().toFixed(2) + ' K';
+    return balance.div(new BN(KILT_FEMTO_COIN)).toNumber() + ' K';
   }
 
   function balanceListener(address: string, balance: BN, change: BN) {
+    console.log('Balance: ', BN);
     console.log('Balance changed by ', asKiltCoins(change));
     setBalance(asKiltCoins(balance));
   }
