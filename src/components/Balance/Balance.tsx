73ea0c2323bad15d27b36ce96a24cf6c2e8d6568
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2021-03-11 12:04:34 +0100

    made suggested changes

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
-import {
-  getBalance,
-  listenToBalanceChanges,
-} from '@kiltprotocol/core/lib/balance/Balance.chain';
+import { listenToBalanceChanges } from '@kiltprotocol/core/lib/balance/Balance.chain';
 import BN from 'bn.js';
 import { ClipLoader } from 'react-spinners';
 
 const KILT_FEMTO_COIN = 1e15; // or 1_000_000_000_000_000
+
+function asKiltCoins(balance: BN): string {
+  return balance.div(new BN(KILT_FEMTO_COIN)).toNumber() + ' K';
+}
 
 interface BalanceProps {
   address: string;
@@ -14,10 +15,6 @@ interface BalanceProps {
 
 export function Balance({ address }: BalanceProps): JSX.Element {
   const [balance, setBalance] = useState('');
-
-  function asKiltCoins(balance: BN): string {
-    return balance.div(new BN(KILT_FEMTO_COIN)).toNumber() + ' K';
-  }
 
   function balanceListener(address: string, balance: BN, change: BN) {
     console.log('Balance: ', BN);
@@ -30,11 +27,7 @@ export function Balance({ address }: BalanceProps): JSX.Element {
     (async () => {
       // listen for balance changes
       unsubscribe = await listenToBalanceChanges(address, balanceListener);
-      // get balance
-      const balanceRaw = await getBalance(address);
-      setBalance(asKiltCoins(balanceRaw));
     })();
-    // stop listening on unmount
     return () => {
       unsubscribe();
     };
