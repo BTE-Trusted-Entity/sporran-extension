e0c79e1d76f21c5b7f34f04d9dbde8c5d3c1d77a
Tom Adler <me@arty.name>
2021-03-12 12:03:01 +0100

    configured storybook decorators

---
+++ .storybook/preview.jsx
@@ -0,0 +1,37 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { withConsole, setConsoleOptions } from '@storybook/addon-console';
+import { withInfo } from '@storybook/addon-info';
+import { MemoryRouter } from 'react-router-dom';
+
+// You'll start to receive all console messages, warnings, errors in your action logger panel - Everything except HMR logs.
+setConsoleOptions({
+  panelExclude: []
+});
+
+export const decorators = [
+  withInfo,
+
+  // You'll receive console outputs as a console,
+  // warn and error actions in the panel. You might want to know from
+  // what stories they come. In this case, add withConsole decorator:
+  (storyFn, context) => withConsole()(storyFn)(context),
+
+  (Story) => <MemoryRouter><Story /></MemoryRouter>,
+];
+
+export const parameters = {
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+      popup: {
+        name: 'Extension Popup',
+        type: 'desktop',
+        styles: {
+          height: '640px',
+          width: '480px',
+        }
+      }
+    },
+    defaultViewport: 'popup',
+  }
+};
