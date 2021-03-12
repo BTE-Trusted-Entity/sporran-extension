b7967f1fc4a6be26c70db0943b9c85e530dbdfb2
Luke Grunau <lukegrunau@gmail.com>
2021-03-12 18:22:16 +0100

    SK-18 - Added Warning view (#14)

---
+++ src/views/Warning/Warning.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from '@storybook/react';
+import { Warning } from './Warning';
+
+export default {
+  title: 'Views/Warning',
+  component: Warning,
+} as Meta;
+
+export function Template(): JSX.Element {
+  return <Warning />;
+}
---
+++ src/views/Warning/Warning.tsx
@@ -0,0 +1,29 @@
+import { browser } from 'webextension-polyfill-ts';
+import { Link } from 'react-router-dom';
+
+import styles from './Warning.module.css';
+
+export function Warning(): JSX.Element {
+  const t = browser.i18n.getMessage;
+
+  return (
+    <div className={styles.container}>
+      <Link to="/" className={styles.backButton}>
+        {t('common_action_back')}
+      </Link>
+      <h1>{t('view_Warning_headline')}</h1>
+      <div>
+        <h3>[Insert logo here]</h3>
+      </div>
+      <h2>{t('view_Warning_emphasis')}</h2>
+      <p>{t('view_Warning_explanation')}</p>
+      <h2>{t('view_Warning_emphasis_again')}</h2>
+      <p>
+        <Link to="/account/create/verify">{t('view_Warning_CTA')}</Link>
+      </p>
+      <p>
+        <Link to="/">{t('common_action_cancel')}</Link>
+      </p>
+    </div>
+  );
+}
