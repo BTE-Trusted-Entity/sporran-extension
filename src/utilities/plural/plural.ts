5d7f80751e7e17c5462a71dd8fb9915b3b6a243e
Tom Adler <me@arty.name>
2021-03-17 16:54:45 +0100

    added small pluralization utility

---
+++ src/utilities/plural/plural.ts
@@ -0,0 +1,22 @@
+import { browser } from 'webextension-polyfill-ts';
+
+type Category = 'zero' | 'one' | 'two' | 'few' | 'many' | 'other';
+
+type Keys = {
+  [category in Category]?: string;
+};
+
+export function plural(value: number, keys: Keys): string {
+  const locale = browser.i18n.getUILanguage();
+  const category = new Intl.PluralRules(locale).select(value);
+
+  const key = keys[category];
+  if (!key) {
+    const json = JSON.stringify(keys);
+    throw new Error(
+      `The category ${category} is not present in translation keys ${json}`,
+    );
+  }
+
+  return browser.i18n.getMessage(key);
+}
