2ecbb46f179414f0525f88375721ecd5c9d46cac
Tom Adler <me@arty.name>
2021-03-17 11:40:15 +0100

    SK-27: Success screen for created / imported account (#41)

--- src/views/CreateAccountSuccess/CreateAccountSuccess.test.tsx
+++ src/views/CreateAccountSuccess/CreateAccountSuccess.test.tsx
@@ -2,8 +2,12 @@ import { CreateAccountSuccess } from './CreateAccountSuccess';
 import { render } from '../../testing';
 
 describe('CreateAccountSuccess', () => {
-  it('should render', async () => {
+  it('should render for create', async () => {
     const { container } = render(<CreateAccountSuccess />);
+    expect(container).toMatchSnapshot();
+  });
+  it('should render for import', async () => {
+    const { container } = render(<CreateAccountSuccess type="import" />);
     expect(container).toMatchSnapshot();
   });
 });
