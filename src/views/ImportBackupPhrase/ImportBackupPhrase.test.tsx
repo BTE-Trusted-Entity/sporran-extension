c33763bc1774716af3b2c719996d5dd17e5c6bad
dudleyneedham <dudleyneedham1993@gmail.com>
2021-03-22 17:09:08 +0100

    adding further tests

--- src/views/ImportBackupPhrase/ImportBackupPhrase.test.tsx
+++ src/views/ImportBackupPhrase/ImportBackupPhrase.test.tsx
@@ -15,13 +15,24 @@ describe('ImportBackupPhrase', () => {
     expect(container).toMatchSnapshot();
   });
 
-  it('should enter account', async () => {
+  it('should report backup word not within libary', async () => {
     render(<ImportBackupPhrase {...props} />);
+    userEvent.type(await screen.findByLabelText('1'), 'oooooo');
+    userEvent.click(screen.getByText('Import Account'));
+    expect(onImport).not.toHaveBeenCalled();
+  });
 
+  it('should report backup phrase not long enough', async () => {
+    render(<ImportBackupPhrase {...props} />);
     userEvent.type(await screen.findByLabelText('1'), 'century');
     userEvent.click(screen.getByText('Import Account'));
     expect(onImport).not.toHaveBeenCalled();
+  });
 
+  it('should report invalid backup phrase', async () => {
+    render(<ImportBackupPhrase {...props} />);
+
+    userEvent.type(await screen.findByLabelText('1'), 'century');
     userEvent.type(await screen.findByLabelText('2'), 'answer');
     userEvent.type(await screen.findByLabelText('3'), 'price');
     userEvent.type(await screen.findByLabelText('4'), 'repeat');
@@ -36,10 +47,26 @@ describe('ImportBackupPhrase', () => {
 
     userEvent.click(screen.getByText('Import Account'));
     expect(onImport).not.toHaveBeenCalled();
+  });
 
-    userEvent.clear(await screen.findByLabelText('12'));
+  it('should allow backup phrase', async () => {
+    render(<ImportBackupPhrase {...props} />);
+
+    userEvent.type(await screen.findByLabelText('1'), 'century');
+    userEvent.type(await screen.findByLabelText('2'), 'answer');
+    userEvent.type(await screen.findByLabelText('3'), 'price');
+    userEvent.type(await screen.findByLabelText('4'), 'repeat');
+    userEvent.type(await screen.findByLabelText('5'), 'carpet');
+    userEvent.type(await screen.findByLabelText('6'), 'truck');
+    userEvent.type(await screen.findByLabelText('7'), 'swarm');
+    userEvent.type(await screen.findByLabelText('8'), 'boost');
+    userEvent.type(await screen.findByLabelText('9'), 'fine');
+    userEvent.type(await screen.findByLabelText('10'), 'siege');
+    userEvent.type(await screen.findByLabelText('11'), 'brain');
     userEvent.type(await screen.findByLabelText('12'), 'fog');
     userEvent.click(screen.getByText('Import Account'));
-    expect(onImport).toHaveBeenCalled();
+    expect(onImport).toHaveBeenCalledWith(
+      'century answer price repeat carpet truck swarm boost fine siege brain fog',
+    );
   });
 });
