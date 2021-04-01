87270ec2d39a3a827a0d2a422f1c1005f7b009a1
Tom Adler <me@arty.name>
2021-04-01 13:39:15 +0200

    added LinkBack button with built-in "go back" behaviour

--- src/components/LinkBack/LinkBack.stories.tsx
+++ src/components/LinkBack/LinkBack.stories.tsx
@@ -1,6 +1,8 @@
 import { Meta } from '@storybook/react';
 import { Link } from 'react-router-dom';
 
+import { LinkBack } from './LinkBack';
+
 import styles from './LinkBack.module.css';
 
 export default {
@@ -11,10 +13,18 @@ const background = {
   padding: '2rem',
 };
 
-export function LinkBack(): JSX.Element {
+export function CSS(): JSX.Element {
   return (
     <section style={background}>
       <Link to="" title="Back" className={styles.linkBack} />
+    </section>
+  );
+}
+
+export function Component(): JSX.Element {
+  return (
+    <section style={background}>
+      <LinkBack />
     </section>
   );
 }
