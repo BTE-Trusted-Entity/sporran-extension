f81cb5fb18463c01df059645ebbbadc012b0a2fe
Tom Adler <me@arty.name>
2021-04-06 17:46:15 +0200

    SK-75: Design add account menu

--- src/components/AddAccount/AddAccount.stories.tsx
+++ src/components/AddAccount/AddAccount.stories.tsx
@@ -1,10 +1,21 @@
 import { Meta } from '@storybook/react';
 
+import menuStyles from '../Menu/Menu.module.css';
+
 import { AddAccount } from './AddAccount';
 
 export default {
   title: 'Components/AddAccount',
   component: AddAccount,
+  decorators: [
+    (Story) => (
+      <section style={{ padding: '2rem' }}>
+        <div className={menuStyles.wrapper} style={{ float: 'right' }}>
+          <Story />
+        </div>
+      </section>
+    ),
+  ],
 } as Meta;
 
 export function Template(): JSX.Element {
