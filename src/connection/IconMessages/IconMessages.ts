1d038e8f57a601d754cb32be07f17c8a6806a131
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2021-05-10 19:40:35 +0200

    Design changes

---
+++ src/connection/IconMessages/IconMessages.ts
@@ -0,0 +1,26 @@
+import { browser } from 'webextension-polyfill-ts';
+import { createOnMessage } from '../createOnMessage/createOnMessage';
+
+const toggleIconRequest = 'toggleIconsRequest';
+
+type ToggleIconRequest = Record<string, never>;
+
+export function toggleIcon(): void {
+  if (!window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return;
+  }
+  browser.runtime.sendMessage({
+    type: toggleIconRequest,
+  });
+}
+
+export const onToggleIconRequest = createOnMessage<ToggleIconRequest>(
+  toggleIconRequest,
+);
+
+export async function toggleIconListener(): Promise<void> {
+  console.log('setting dark mode icon');
+  browser.browserAction.setIcon({
+    path: 'icon-dark-mode-16.png',
+  });
+}
