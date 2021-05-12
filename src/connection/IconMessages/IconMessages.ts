d2a641fa34707ff3dc8f3464665aed68ab9f19da
Luke Grunau <lukegrunau@gmail.com>
2021-05-12 15:06:13 +0200

    SK-233: Fix blurry icon (#302) (#302)

--- src/connection/IconMessages/IconMessages.ts
+++ src/connection/IconMessages/IconMessages.ts
@@ -21,6 +21,7 @@ export async function toggleIconListener(): Promise<void> {
   browser.browserAction.setIcon({
     path: {
       16: 'icon/dark/16.png',
+      32: 'icon/dark/32.png',
       48: 'icon/dark/48.png',
       128: 'icon/dark/128.png',
     },
