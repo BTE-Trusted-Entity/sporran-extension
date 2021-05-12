c0b2f3f1de8f64197aef54c8380732a9d800fc52
Tom Adler <me@arty.name>
2021-05-12 18:58:51 +0200

    fixed act warning

--- src/views/AppSettings/AppSettings.test.tsx
+++ src/views/AppSettings/AppSettings.test.tsx
@@ -1,9 +1,15 @@
-import { render } from '../../testing/testing';
+import { render, screen, waitFor } from '../../testing/testing';
 import { AppSettings } from './AppSettings';
 
 describe('AppSettings', () => {
   it('should render', async () => {
     const { container } = render(<AppSettings />);
+
+    await waitFor(async () => {
+      const endpoint = (await screen.getByRole('combobox')) as HTMLInputElement;
+      return endpoint.value !== '';
+    });
+
     expect(container).toMatchSnapshot();
   });
 });
