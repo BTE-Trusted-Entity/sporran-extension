33e858a67abbb0d2a145f190bd7456106c234be8
Angelos Angelides <angelangelides@gmail.com>
2021-05-26 11:14:56 +0200

    chore: revert change on story

--- src/views/ReviewTransaction/ReviewTransaction.stories.tsx
+++ src/views/ReviewTransaction/ReviewTransaction.stories.tsx
@@ -24,7 +24,7 @@ export function Template(): JSX.Element {
         <ReviewTransaction
           account={accounts['4tJbxxKqYRv3gDvY66BKyKzZheHEH8a27VBiMfeGX2iQrire']}
           recipient="4p1VA6zuhqKuZ8EdJA7QtjcB9mVLt3L31EKWVXfbJ6GaiQos"
-          amount={new BN((121251241231231231).toString())}
+          amount={new BN((120e15).toString())}
           fee={new BN(1.25e7)}
           tip={new BN(0.01e15)}
           onSuccess={action('success')}
