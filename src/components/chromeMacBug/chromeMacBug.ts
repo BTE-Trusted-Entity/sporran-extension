3b23b30dd0eaecae6b2de718fd497cdcdcce751b
Tom Adler <me@arty.name>
2021-06-03 17:14:35 +0200

    SK-242: Extension sometimes unresponsive on an external display

---
+++ src/components/chromeMacBug/chromeMacBug.ts
@@ -0,0 +1,27 @@
+import { browser } from 'webextension-polyfill-ts';
+
+import './chromeMacBug.css';
+
+const { devicePixelRatio, screen, screenTop, screenLeft } = window;
+
+const displayHasLowDpi = devicePixelRatio < 2;
+
+const displayIsSecondary =
+  screenLeft < 0 ||
+  screenTop < 0 ||
+  screenLeft > screen.width ||
+  screenTop > screen.height;
+
+/**
+ * This is a workaround for https://bugs.chromium.org/p/chromium/issues/detail?id=971701
+ * Chrome@Mac on a secondary display with DPI less than on main display freezes the popup.
+ * Repainting every second alleviates the problem.
+ */
+export async function chromeMacBug(): Promise<void> {
+  const { os } = await browser.runtime.getPlatformInfo();
+  const isMac = os === 'mac';
+
+  if (isMac && displayIsSecondary && displayHasLowDpi) {
+    document.documentElement.classList.add('redraw-forever');
+  }
+}
