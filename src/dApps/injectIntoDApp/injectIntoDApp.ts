ad226b162c9231e92c10c9ad4a0c1ca649fb2ecc
Tom Adler <me@arty.name>
2021-06-03 14:27:22 +0200

    SK-267: Implement Feedback from SRL Security Audit (#370)

--- src/dApps/injectIntoDApp/injectIntoDApp.ts
+++ src/dApps/injectIntoDApp/injectIntoDApp.ts
@@ -3,6 +3,11 @@ import { injectExtension } from '@polkadot/extension-inject';
 import { injectedAccessChannel } from '../AccessChannels/injectedAccessChannel';
 import { AccountsInjectedAPI } from '../AccountsInjectedAPI/AccountsInjectedAPI';
 import { SignerInjectedAPI } from '../SignerInjectedAPI/SignerInjectedAPI';
+import { debounceAsync } from '../../utilities/debounceAsync/debounceAsync';
+
+const authenticate = debounceAsync<typeof injectedAccessChannel.get>((input) =>
+  injectedAccessChannel.get(input),
+);
 
 export function injectIntoDApp(): void {
   const sporranMeta = {
@@ -10,8 +15,9 @@ export function injectIntoDApp(): void {
     version: '1.0.0', // TODO: version
   };
 
-  injectExtension(async (dAppName: string) => {
-    await injectedAccessChannel.get({ dAppName });
+  injectExtension(async (unsafeDAppName: string) => {
+    const dAppName = unsafeDAppName.substring(0, 50);
+    await authenticate({ dAppName });
 
     return {
       accounts: new AccountsInjectedAPI(dAppName),
---
+++ src/utilities/debounceAsync/debounceAsync.test.ts
@@ -0,0 +1,26 @@
+import { debounceAsync } from './debounceAsync';
+
+const callback = jest.fn().mockImplementation(async (i: number) => i * 2);
+const debounced = debounceAsync<typeof callback>(callback);
+
+describe('debounceAsync', () => {
+  it('should return results normally', async () => {
+    callback.mockClear();
+    expect(await debounced(1)).toEqual(2);
+  });
+
+  it('should prevent parallel requests', async () => {
+    callback.mockClear();
+
+    debounced(1);
+    await debounced(1);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should allow sequential calls', async () => {
+    callback.mockClear();
+    expect(await debounced(1)).toEqual(2);
+    expect(await debounced(2)).toEqual(4);
+    expect(callback).toHaveBeenCalledTimes(2);
+  });
+});
