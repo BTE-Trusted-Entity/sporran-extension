96d72df81774d9566593e077a90f06f8bb39b4cf
Tom Adler <me@arty.name>
2021-06-07 10:07:30 +0200

    SK-269: Balances of multiple accounts are mixed up (quick fix) (#378)

--- src/channels/balanceChangeChannel/mockBalanceChangeChannel.ts
+++ src/channels/balanceChangeChannel/mockBalanceChangeChannel.ts
@@ -5,10 +5,13 @@ import { balanceMock } from './balanceChangeChannel.mock';
 jest.mock('./balanceChangeChannel');
 
 const bnBalanceMock = {
-  bonded: new BN(balanceMock.bonded),
-  free: new BN(balanceMock.free),
-  locked: new BN(balanceMock.locked),
-  total: new BN(balanceMock.total),
+  address: '4tJbxxKqYRv3gDvY66BKyKzZheHEH8a27VBiMfeGX2iQrire',
+  balances: {
+    bonded: new BN(balanceMock.bonded),
+    free: new BN(balanceMock.free),
+    locked: new BN(balanceMock.locked),
+    total: new BN(balanceMock.total),
+  },
 };
 
 export function mockBalanceChangeChannel(): void {
