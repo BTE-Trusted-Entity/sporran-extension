4e91b7f490cf9ecd629c32bc568c70b65e4c17b9
Luke Grunau <lukegrunau@gmail.com>
2021-06-10 17:02:50 +0200

    Fix total balance (#403)

--- src/channels/balanceChangeChannel/balanceChangeChannel.test.ts
+++ src/channels/balanceChangeChannel/balanceChangeChannel.test.ts
@@ -11,9 +11,9 @@ jest.unmock('./balanceChangeChannel');
 
 const originalBalancesMock = {
   free: new BN(1.226e15),
-  reserved: new BN(0.005e15),
-  miscFrozen: new BN(0.002e15),
-  feeFrozen: new BN(0.001e15),
+  miscFrozen: new BN(0.004e15),
+  feeFrozen: new BN(0.006e15),
+  reserved: new BN(0.008e15),
 };
 
 describe('balanceChangeChannel', () => {
