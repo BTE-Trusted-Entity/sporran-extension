ac6f9743f22a2db519c1ca82ea413446eceb38c7
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2021-06-14 09:27:50 +0200

    updated balance mock

--- src/channels/balanceChangeChannel/balanceChangeChannel.mock.ts
+++ src/channels/balanceChangeChannel/balanceChangeChannel.mock.ts
@@ -4,9 +4,9 @@ import { JsonChangeOutput, Balances } from './balanceChangeChannel';
 
 export const originalBalancesMock: Balances = {
   free: new BN(1.226e15),
-  reserved: new BN(0.005e15),
-  miscFrozen: new BN(0.002e15),
-  feeFrozen: new BN(0.001e15),
+  miscFrozen: new BN(0.004e15),
+  feeFrozen: new BN(0.006e15),
+  reserved: new BN(0.008e15),
 };
 
 export const balanceMock: JsonChangeOutput['balances'] = {
