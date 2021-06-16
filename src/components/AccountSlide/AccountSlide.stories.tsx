6044f1fa5b9645d40acf374300665f8556a5d0b2
Luke Grunau <lukegrunau@gmail.com>
2021-06-16 16:49:41 +0200

    SK-78: Long name overflow (#414)

--- src/components/AccountSlide/AccountSlide.stories.tsx
+++ src/components/AccountSlide/AccountSlide.stories.tsx
@@ -3,6 +3,8 @@ import { Meta } from '@storybook/react';
 import { AccountSlide } from './AccountSlide';
 import { AccountSlideNew } from './AccountSlideNew';
 
+import { accountsMock as accounts } from '../../utilities/accounts/AccountsProvider.mock';
+
 export default {
   title: 'Components/AccountSlide',
   component: AccountSlide,
@@ -12,12 +14,15 @@ export default {
 export function Template(): JSX.Element {
   return (
     <AccountSlide
-      account={{
-        name: 'My Sporran Account',
-        tartan: 'MacPherson',
-        address: '4tJbxxKqYRv3gDvY66BKyKzZheHEH8a27VBiMfeGX2iQrire',
-        index: 1,
-      }}
+      account={accounts['4tJbxxKqYRv3gDvY66BKyKzZheHEH8a27VBiMfeGX2iQrire']}
+    />
+  );
+}
+
+export function LongName(): JSX.Element {
+  return (
+    <AccountSlide
+      account={accounts['4oyRTDhHL22Chv9T89Vv2TanfUxFzBnPeMuq4EFL3gUiHbtL']}
     />
   );
 }
