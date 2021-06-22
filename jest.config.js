41aef68607b4552ae19e0ec379131a03ffee2c3d
Tom Adler <me@arty.name>
2021-06-22 17:49:26 +0200

    SK-311: Change to standard identicon (#440)

--- jest.config.js
+++ jest.config.js
@@ -116,9 +116,10 @@ export default {
   transform: {
     '\\.tsx?$': 'ts-jest',
     'node_modules/lodash-es/.*': 'babel-jest',
+    'node_modules/@polkadot/.*': 'babel-jest',
   },
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['/node_modules/(?!lodash-es)'],
+  transformIgnorePatterns: ['/node_modules/(?!(lodash-es)|(@polkadot))'],
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
   // Indicates whether each individual test should be reported during the run
