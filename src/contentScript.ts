d25ec0874e1ce0ee54f93c9c2bd9665bf625cb74
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2021-08-12 11:51:11 +0200

    SK-400: Only expose API if at least one identity is configured

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,12 +1,19 @@
 import { browser } from 'webextension-polyfill-ts';
+import { getIdentities } from './utilities/identities/getIdentities';
 import { toggleIcon } from './channels/toggleIconChannel/toggleIconChannel';
 import { initContentIdentitiesChannel } from './dApps/identitiesDataProvider/identitiesDataProvider';
 import { initContentAccessChannel } from './dApps/checkAccess/checkAccess';
 import { initContentSignChannel } from './dApps/SignChannels/contentSignChannel';
 import { initContentCredentialChannel } from './channels/CredentialChannels/contentCredentialChannel';
 
-function injectScript() {
+async function injectScript() {
   // content scripts cannot expose APIs to website code, only injected scripts can
+
+  const identities = await getIdentities();
+  if (!Object.keys(identities).length) {
+    return;
+  }
+
   const script = document.createElement('script');
   script.type = 'module';
   script.async = true;
