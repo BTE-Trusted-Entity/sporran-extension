7a0d1cb28897581bcbc4653fce7b91522c17981a
Luke Grunau <lukegrunau@lukes-mbp.speedport.ip>
2021-08-13 14:03:47 +0200

    useInterval test

--- src/utilities/useInterval/useInterval.ts
+++ src/utilities/useInterval/useInterval.ts
@@ -1,26 +1,10 @@
-// https://gist.github.com/babakness/faca3b633bc23d9a0924efb069c9f1f5
-// Typescript version of Dan Abramov's useInterval
-
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
 type IntervalFunction = () => unknown | void;
 
 export function useInterval(callback: IntervalFunction, delay: number): void {
-  const savedCallback = useRef<IntervalFunction | null>(null);
-
-  // Remember the latest callback.
   useEffect(() => {
-    savedCallback.current = callback;
-  });
-
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      if (savedCallback.current !== null) {
-        savedCallback.current();
-      }
-    }
-    const id = setInterval(tick, delay);
+    const id = setInterval(callback, delay);
     return () => clearInterval(id);
-  }, [delay]);
+  }, [callback, delay]);
 }
