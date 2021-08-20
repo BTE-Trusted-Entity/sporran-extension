8bb19fdaeee96c11af356b2a560258f5c1238b50
Luke Grunau <lukegrunau@lukes-mbp.speedport.ip>
2021-08-20 11:34:17 +0200

    SK-405: Fixed and simplified Sign Channels

--- src/dApps/SignChannels/contentSignChannel.ts
+++ src/dApps/SignChannels/contentSignChannel.ts
@@ -8,45 +8,11 @@ import { injectedSignChannel } from './injectedSignChannel';
 
 type SignInput = SignerPayloadJSON & { origin: string };
 
-type SignJsonInput = {
-  version: string;
-  signedExtensions: string;
-} & Omit<SignInput, 'version' | 'signedExtensions'>;
-
 type SignOutput = SignerResult;
 
-interface SignJsonOutput {
-  id: string;
-  signature: string;
-}
-
-const transform = {
-  inputToJson: (input: SignInput) => ({
-    ...input,
-    version: String(input.version),
-    signedExtensions: JSON.stringify(input.signedExtensions),
-  }),
-  jsonToInput: (input: SignJsonInput) => ({
-    ...input,
-    version: parseInt(input.version),
-    signedExtensions: JSON.parse(input.signedExtensions),
-  }),
-  outputToJson: ({ id, signature }: SignOutput) => ({
-    id: String(id),
-    signature,
-  }),
-  jsonToOutput: ({ id, signature }: SignJsonOutput) => ({
-    id: parseInt(id),
-    signature,
-  }),
-};
-
-export const contentSignChannel = new BrowserChannel<
-  SignInput,
-  SignOutput,
-  SignJsonInput,
-  SignJsonOutput
->('dAppSign', false, transform);
+export const contentSignChannel = new BrowserChannel<SignInput, SignOutput>(
+  'dAppSign',
+);
 
 export function initContentSignChannel(origin: string): () => void {
   return injectedSignChannel.produce(async ({ dAppName, payload }) => {
--- src/views/SignDApp/SignDApp.test.tsx
+++ src/views/SignDApp/SignDApp.test.tsx
@@ -2,19 +2,32 @@ import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '../../testing/testing';
 import '../../components/useCopyButton/useCopyButton.mock';
+import { jsonToBase64 } from '../../utilities/popups/usePopupData';
 import { paths } from '../paths';
 
 import { SignDApp } from './SignDApp';
 
 jest.mock('@kiltprotocol/chain-helpers', () => ({}));
 
-const query =
-  'address=4tJbxxKqYRv3gDvY66BKyKzZheHEH8a27VBiMfeGX2iQrire&specVersion=1&nonce=1&method=namespace.method(input = "some meaningful values you would definitely like to see")&lifetimeStart=1&lifetimeEnd=1000000&origin=https://example.com/extremely-long-url-tries-to-overflow-all-available-space';
+const mockExtrinsic = {
+  origin: 'example.com',
+  address: '4tJbxxKqYRv3gDvY66BKyKzZheHEH8a27VBiMfeGX2iQrire',
+  specVersion: 1,
+  nonce: 1,
+  method:
+    'namespace.method(input = "some meaningful values you would definitely like to see")',
+  lifetimeStart: 1,
+  lifetimeEnd: 1000000,
+};
+
+const encodedData = jsonToBase64(mockExtrinsic);
 
 describe('SignDApp', () => {
   it('should render', async () => {
     const { container } = render(
-      <MemoryRouter initialEntries={[`${paths.popup.sign}?${query}`]}>
+      <MemoryRouter
+        initialEntries={[`${paths.popup.sign}?data=${encodedData}`]}
+      >
         <SignDApp />
       </MemoryRouter>,
     );
