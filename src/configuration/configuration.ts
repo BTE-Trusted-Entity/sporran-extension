4b4e12a7956de56fcd845a75d00d24c2a7fe76b4
Tom Adler <me@arty.name>
2021-09-08 11:37:39 +0200

    new endpoint and version

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -28,7 +28,7 @@ export const internalFeatures: Features = {
 
 // Duplicates the value in src/static/manifest.json
 // We can’t use browser.runtime.getManifest().version, as it’s unavailable in injected scripts
-const version = '2021.9.3';
+const version = '2021.9.8';
 
 export const configuration: ConfigurationType = {
   version,
