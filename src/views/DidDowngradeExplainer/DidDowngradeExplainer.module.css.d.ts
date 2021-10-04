ab4b2a5f353d80a74d939348b218cfc61be24e09
Luke Grunau <lukegrunau@lukes-mbp.speedport.ip>
2021-10-04 13:57:56 +0200

    SK-461: Downgrade DID Explainer

---
+++ src/views/DidDowngradeExplainer/DidDowngradeExplainer.module.css.d.ts
@@ -0,0 +1,18 @@
+declare namespace DidDowngradeExplainerModuleCssNamespace {
+  export interface IDidDowngradeExplainerModuleCss {
+    container: string;
+    continue: string;
+    explanation: string;
+    heading: string;
+    info: string;
+    subline: string;
+    warning: string;
+  }
+}
+
+declare const DidDowngradeExplainerModuleCssModule: DidDowngradeExplainerModuleCssNamespace.IDidDowngradeExplainerModuleCss & {
+  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
+  locals: DidDowngradeExplainerModuleCssNamespace.IDidDowngradeExplainerModuleCss;
+};
+
+export = DidDowngradeExplainerModuleCssModule;
