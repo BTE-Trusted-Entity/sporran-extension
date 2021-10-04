4688e96a3b880d42146d51052d0c0b85f9b30a65
Luke Grunau <lukegrunau@lukes-mbp.speedport.ip>
2021-10-04 14:31:39 +0200

    Suggested changes

--- src/views/DidDowngradeExplainer/DidDowngradeExplainer.stories.tsx
+++ src/views/DidDowngradeExplainer/DidDowngradeExplainer.stories.tsx
@@ -1,8 +1,6 @@
 import { Meta } from '@storybook/react';
-import { MemoryRouter, Route } from 'react-router-dom';
 
 import { identitiesMock as identities } from '../../utilities/identities/IdentitiesProvider.mock';
-import { paths } from '../paths';
 
 import { DidDowngradeExplainer } from './DidDowngradeExplainer';
 
@@ -13,18 +11,8 @@ export default {
 
 export function Template(): JSX.Element {
   return (
-    <MemoryRouter
-      initialEntries={[
-        '/identity/4tJbxxKqYRv3gDvY66BKyKzZheHEH8a27VBiMfeGX2iQrire/did/downgrade',
-      ]}
-    >
-      <Route path={paths.identity.did.downgrade.start}>
-        <DidDowngradeExplainer
-          identity={
-            identities['4sm9oDiYFe22D7Ck2aBy5Y2gzxi2HhmGML98W9ZD2qmsqKCr']
-          }
-        />
-      </Route>
-    </MemoryRouter>
+    <DidDowngradeExplainer
+      identity={identities['4sm9oDiYFe22D7Ck2aBy5Y2gzxi2HhmGML98W9ZD2qmsqKCr']}
+    />
   );
 }
--- src/views/DidUpgradeExplainer/DidUpgradeExplainer.stories.tsx
+++ src/views/DidUpgradeExplainer/DidUpgradeExplainer.stories.tsx
@@ -1,8 +1,6 @@
-import { MemoryRouter, Route } from 'react-router-dom';
 import { Meta } from '@storybook/react';
 
 import { identitiesMock as identities } from '../../utilities/identities/IdentitiesProvider.mock';
-import { paths } from '../paths';
 
 import { DidUpgradeExplainer } from './DidUpgradeExplainer';
 
@@ -13,18 +11,8 @@ export default {
 
 export function Template(): JSX.Element {
   return (
-    <MemoryRouter
-      initialEntries={[
-        '/identity/4tJbxxKqYRv3gDvY66BKyKzZheHEH8a27VBiMfeGX2iQrire/did/upgrade',
-      ]}
-    >
-      <Route path={paths.identity.did.upgrade.start}>
-        <DidUpgradeExplainer
-          identity={
-            identities['4tJbxxKqYRv3gDvY66BKyKzZheHEH8a27VBiMfeGX2iQrire']
-          }
-        />
-      </Route>
-    </MemoryRouter>
+    <DidUpgradeExplainer
+      identity={identities['4tJbxxKqYRv3gDvY66BKyKzZheHEH8a27VBiMfeGX2iQrire']}
+    />
   );
 }
