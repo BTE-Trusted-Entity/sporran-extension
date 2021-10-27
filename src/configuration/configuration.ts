0c4a9de0a99ed75752ae20bc7486c92c9d047d92
Tom Adler <me@arty.name>
2021-10-27 13:41:33 +0200

    Sporran version bump

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -28,7 +28,7 @@ export const internalFeatures: Features = {
 
 // Duplicates the value in src/static/manifest.json
 // We can’t use browser.runtime.getManifest().version, as it’s unavailable in injected scripts
-const version = '2021.9.8';
+const version = '2021.10.27';
 
 export const configuration: ConfigurationType = {
   version,
