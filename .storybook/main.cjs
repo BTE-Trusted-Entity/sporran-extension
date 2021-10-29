c08717ff39d9672873fb51defb7e98d67e9327c5
Tom Adler <me@arty.name>
2021-10-29 15:33:53 +0200

    fixed storybook styles for named exports

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -12,10 +12,19 @@ module.exports = {
     '@storybook/addon-controls/register',
     '@storybook/addon-toolbars/register',
   ],
-  typescript: {
-    reactDocgen: 'none', // current version doesn’t work with recent TS
-  },
   webpackFinal: async (config) => {
+    const cssRule = config.module.rules.find(({ test }) => test.toString() === '/\\.css$/');
+
+    // needs to be done while Storybook uses style-loader@2
+    const styleLoaderOptions = {
+      loader: cssRule.use[0],
+      options: { modules: { namedExport: true } }
+    };
+    cssRule.use[0] = styleLoaderOptions;
+
+    const cssLoaderOptions = cssRule.use[1].options;
+    cssLoaderOptions.modules = { namedExport: true };
+
     config.resolve.alias = {
       ...config.resolve.alias,
       'webextension-polyfill-ts': require.resolve('../src/__mocks__/webextension-polyfill-ts.ts'),
