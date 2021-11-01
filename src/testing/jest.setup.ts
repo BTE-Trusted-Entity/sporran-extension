027a88e45849c7e99b42bdd0dd9e69521c382e95
Tom Adler <me@arty.name>
2021-11-01 14:17:38 +0100

    fix tests

--- src/testing/jest.setup.ts
+++ src/testing/jest.setup.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom';
+import { TextDecoder } from 'util';
+
+// The cbor package (or its dependency) needs this to work, and it works in the browser,
+// but not in the jest-provided jsdom environment
+// https://github.com/hildjj/nofilter/issues/7
+// https://github.com/jsdom/jsdom/issues/2524
+if (!('TextDecoder' in global)) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  global.TextDecoder = TextDecoder;
+}
