f5f026327ff6cf899692db7d3939b3ab0b8293c4
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2021-11-04 09:56:50 +0100

    Change default endpoint to peregrine in internal version

--- src/utilities/endpoints/endpoints.ts
+++ src/utilities/endpoints/endpoints.ts
@@ -1,5 +1,6 @@
 import { browser } from 'webextension-polyfill-ts';
 import { storage } from '../storage/storage';
+import { isInternal } from '../../configuration/variant';
 
 const endpointKey = 'endpoints';
 
@@ -10,8 +11,18 @@ export const endpoints = [
   'wss://kilt-peregrine-stg.kilt.io',
 ];
 
+/* Do we already build a production version first and ask QA to test it 
+before publishing it? Or do we suggest they test an internal version, and 
+if it is ok, we package a production one an upload it? We follow the safer 
+approach when releasing a Sporran-only functionality. We use the less safe 
+approach when we need to validate both endpoints, but in this case it is 
+explicitly about both endpoints, so the production will be tested anyway, 
+so there's no real downside. */
+
 export const defaultEndpoint =
-  process.env.NODE_ENV === 'production' ? endpoints[0] : endpoints[2];
+  process.env.NODE_ENV === 'production' || !isInternal
+    ? endpoints[0]
+    : endpoints[2];
 
 export async function getEndpoint(): Promise<string> {
   return (await storage.get(endpointKey))[endpointKey] || defaultEndpoint;
