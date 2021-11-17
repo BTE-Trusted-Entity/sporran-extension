8fa7015ad10f630be8cb7acdd32287d73dc645d3
Tom Adler <me@arty.name>
2021-11-17 14:06:37 +0100

    updated version

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -28,7 +28,7 @@ export const internalFeatures: Features = {
 
 // Duplicates the value in src/static/manifest.json
 // We can’t use browser.runtime.getManifest().version, as it’s unavailable in injected scripts
-const version = '2021.11.9';
+const version = '2021.11.17';
 
 export const configuration: ConfigurationType = {
   version,
