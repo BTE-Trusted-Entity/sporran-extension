0862789eb8321b72176e16cb2e36ca705930491c
Tom Adler <me@arty.name>
2022-01-28 10:26:55 +0100

    minor cleanup (#1127)

--- src/components/Overlay/Overlay.stories.tsx
+++ src/components/Overlay/Overlay.stories.tsx
@@ -2,7 +2,7 @@ import { action } from '@storybook/addon-actions';
 import { Meta } from '@storybook/react';
 import { Modal } from 'react-dialog-polyfill';
 
-import overlayStyles from './Overlay.module.css';
+import * as overlayStyles from './Overlay.module.css';
 
 export default {
   title: 'Components/Overlay',
