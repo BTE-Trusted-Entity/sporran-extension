2277aefafbe1658e5d25b3c2e238bc3297322d3a
Tom Adler <me@arty.name>
2022-03-02 10:43:48 +0100

    Version 2022.3.2

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -25,7 +25,7 @@ export const internalFeatures: Features = {
 
 // Duplicates the value in src/static/manifest.json
 // We can’t use browser.runtime.getManifest().version, as it’s unavailable in injected scripts
-const version = '2022.2.3';
+const version = '2022.3.2';
 
 export const configuration: ConfigurationType = {
   version,
