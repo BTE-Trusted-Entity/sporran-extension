2490fbbadbdc78222a34fb0b6207d053deb377e2
Luke Grunau <lukegrunau@gmail.com>
2022-03-29 13:21:08 +0200

    SK-871: promo screens costs (#1286)

--- src/views/DidUpgradePromo/DidUpgradePromo.module.css.d.ts
+++ src/views/DidUpgradePromo/DidUpgradePromo.module.css.d.ts
@@ -2,6 +2,7 @@ export const __esModule: true;
 export const container: string;
 export const heading: string;
 export const subline: string;
+export const costs: string;
 export const info: string;
 export const buttonsLine: string;
 export const cancel: string;
