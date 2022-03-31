cc0b1b5a1e57fdb4cd941ab7ee7791a00b0a2256
Luke Grunau <lukegrunau@lukes-mbp.speedport.ip>
2022-03-31 13:25:44 +0200

    improvement

--- src/utilities/promoBackend/promoBackend.ts
+++ src/utilities/promoBackend/promoBackend.ts
@@ -7,16 +7,14 @@ import { getEndpoint } from '../endpoints/endpoints';
 
 const backendOrigins: Record<string, string> = {
   'wss://spiritnet.api.onfinality.io/public-ws':
-    'https://did-promo.sporran.org',
-  'wss://spiritnet.kilt.io': 'https://did-promo.sporran.org',
-  'wss://sporran-testnet.kilt.io': 'https://testnet-did-promo.sporran.org',
+    'https://did-promo.sporran.org/',
+  'wss://spiritnet.kilt.io': 'https://did-promo.sporran.org/',
+  'wss://sporran-testnet.kilt.io': 'https://testnet-did-promo.sporran.org/',
 };
 
-let backendOrigin: string;
-
-async function setOrigin() {
+async function getOrigin() {
   const kiltEndpoint = await getEndpoint();
-  backendOrigin = backendOrigins[kiltEndpoint];
+  return ky.create({ prefixUrl: backendOrigins[kiltEndpoint] });
 }
 
 export async function getPromoStatus(): Promise<{
@@ -24,39 +22,28 @@ export async function getPromoStatus(): Promise<{
   remaining_dids: number;
   is_active: boolean;
 }> {
-  await setOrigin();
-
-  return await ky.get(`${backendOrigin}/promo_status`).json();
+  return (await getOrigin()).get('promo_status').json();
 }
 
 export async function createDid(input: {
   creationDetails: HexString;
   signature: HexString;
 }): Promise<{ tx_hash: HexString }> {
-  await setOrigin();
-
-  return ky.post(`${backendOrigin}/create_did`, { json: input }).json();
+  return (await getOrigin()).get('create_did', { json: input }).json();
 }
 
 export async function submitDidCall(
   extrinsic: SubmittableExtrinsic,
 ): Promise<{ tx_hash: HexString }> {
-  await setOrigin();
-
   const input = {
     call: extrinsic.args[0].toHex(),
     signature: extrinsic.args[1].toHex(),
   };
-  return ky.post(`${backendOrigin}/submit_did_call`, { json: input }).json();
+  return (await getOrigin()).get('submit_did_call', { json: input }).json();
 }
 
 export async function waitFinalized(tx_hash: HexString): Promise<boolean> {
-  await setOrigin();
-
-  return ky
-    .get(`${backendOrigin}/wait_finalized`, {
-      searchParams: { tx_hash },
-      timeout: false,
-    })
+  return (await getOrigin())
+    .get('wait_finalized', { searchParams: { tx_hash }, timeout: false })
     .json();
 }
