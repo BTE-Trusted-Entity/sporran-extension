89c863bd4e8a8fea3799166c8cdf5884aae1b643
Luke Grunau <lukegrunau@gmail.com>
2022-04-05 10:53:33 +0200

    SK-901: Update costs for removing promo on-chain DID (#1320)

--- src/views/DidDowngrade/DidDowngrade.test.tsx
+++ src/views/DidDowngrade/DidDowngrade.test.tsx
@@ -9,6 +9,8 @@ import { mockIsFullDid } from '../../utilities/did/did.mock';
 
 import { parseDidUri } from '../../utilities/did/did';
 
+import { useSwrDataOrThrow } from '../../utilities/useSwrDataOrThrow/useSwrDataOrThrow';
+
 import { DidDowngrade } from './DidDowngrade';
 
 jest.mock('../../utilities/didDowngrade/didDowngrade', () => ({
@@ -21,11 +23,35 @@ jest.mocked(getDeposit).mockResolvedValue(BalanceUtils.toFemtoKilt(1));
 jest.mock('../../utilities/did/did');
 jest.mocked(parseDidUri).mockReturnValue({
   fullDid: 'did:kilt:4rrkiRTZgsgxjJDFkLsivqqKTqdUTuxKk3FX3mKFAeMxsR51',
+  identifier: '4rrkiRTZgsgxjJDFkLsivqqKTqdUTuxKk3FX3mKFAeMxsR51',
 } as ReturnType<typeof parseDidUri>);
 
+jest.mock('../../utilities/useSwrDataOrThrow/useSwrDataOrThrow');
+
 describe('DidDowngrade', () => {
-  it('should render', async () => {
+  it('should show refund amount including deposit if promo was not used', async () => {
     mockIsFullDid(true);
+    jest.mocked(useSwrDataOrThrow).mockReturnValue({
+      deposit: { owner: '4sm9oDiYFe22D7Ck2aBy5Y2gzxi2HhmGML98W9ZD2qmsqKCr' },
+    });
+
+    const { container } = render(
+      <DidDowngrade
+        identity={
+          identities['4sm9oDiYFe22D7Ck2aBy5Y2gzxi2HhmGML98W9ZD2qmsqKCr']
+        }
+      />,
+    );
+    await waitForGetPassword();
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should show only fee amount if promo was used', async () => {
+    mockIsFullDid(true);
+    jest.mocked(useSwrDataOrThrow).mockReturnValue({
+      deposit: { owner: 'some other deposit owner' },
+    });
+
     const { container } = render(
       <DidDowngrade
         identity={
