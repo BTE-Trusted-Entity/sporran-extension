b232f01d41e5d8772b3bc8e05d15ff6a305cf7b0
Luke Grunau <lukegrunau@lukes-mbp.speedport.ip>
2022-04-07 13:25:38 +0200

    Version 2022.4.7

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -19,7 +19,7 @@ export const internalFeatures: Features = {
 
 // Duplicates the value in src/static/manifest.json
 // We can’t use browser.runtime.getManifest().version, as it’s unavailable in injected scripts
-const version = '2022.3.9';
+const version = '2022.4.7';
 
 export const configuration: ConfigurationType = {
   version,
