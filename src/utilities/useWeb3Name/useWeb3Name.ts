b582cd85cc5dcda71ffe6b584a511a45c0c30dd8
Tom Adler <me@arty.name>
2022-04-12 11:45:23 +0200

    SK-927: base58 character 0

--- src/utilities/useWeb3Name/useWeb3Name.ts
+++ src/utilities/useWeb3Name/useWeb3Name.ts
@@ -1,8 +1,10 @@
 import useSWR from 'swr';
 import { Web3Names } from '@kiltprotocol/did';
 
+import { isFullDid } from '../did/did';
+
 export function useWeb3Name(did: string): string | null | undefined {
   return useSWR([did, 'Web3Names.queryWeb3NameForDid'], (did) =>
-    Web3Names.queryWeb3NameForDid(did),
+    isFullDid(did) ? Web3Names.queryWeb3NameForDid(did) : null,
   ).data;
 }
