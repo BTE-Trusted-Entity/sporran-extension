0960d463f76535a6278f9370d927f3e2c123080a
Tom Adler <me@arty.name>
2022-04-27 16:55:52 +0200

    SK-977: base64 vs Unicode (#1395)

---
+++ src/utilities/base64/base64.ts
@@ -0,0 +1,23 @@
+import { AnyJson } from '@polkadot/types/types';
+import { TextDecoder } from '@polkadot/x-textdecoder';
+import { TextEncoder } from '@polkadot/x-textencoder';
+
+export function stringToBase64(input: string): string {
+  const bytes = new TextEncoder().encode(input);
+  const asciiString = String.fromCodePoint(...bytes);
+  return window.btoa(asciiString);
+}
+
+export function base64ToString(base64: string): string {
+  const asciiString = window.atob(base64);
+  const bytes = Uint8Array.from(asciiString, (c) => c.charCodeAt(0));
+  return new TextDecoder().decode(bytes);
+}
+
+export function jsonToBase64(object: unknown): string {
+  return stringToBase64(JSON.stringify(object));
+}
+
+export function base64ToJson(base64: string): AnyJson {
+  return JSON.parse(base64ToString(base64));
+}
--- src/utilities/popups/usePopupData.ts
+++ src/utilities/popups/usePopupData.ts
@@ -1,15 +1,7 @@
 import { useMemo } from 'react';
-import { AnyJson } from '@polkadot/types/types';
 
 import { useQuery } from '../useQuery/useQuery';
-
-export function jsonToBase64(object: AnyJson): string {
-  return window.btoa(JSON.stringify(object));
-}
-
-export function base64ToJson(base64: string): AnyJson {
-  return JSON.parse(window.atob(base64));
-}
+import { base64ToJson } from '../base64/base64';
 
 export function usePopupData<Output>(): Output {
   const { data } = useQuery();
