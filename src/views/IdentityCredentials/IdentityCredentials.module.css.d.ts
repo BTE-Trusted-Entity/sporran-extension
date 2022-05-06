bacf139a71d26aabc8d88d8ce5eee31564278868
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2022-05-06 18:59:15 +0200

    SK-743: Import credentials entry point

--- src/views/IdentityCredentials/IdentityCredentials.module.css.d.ts
+++ src/views/IdentityCredentials/IdentityCredentials.module.css.d.ts
@@ -3,5 +3,7 @@ export const container: string;
 export const heading: string;
 export const subline: string;
 export const credentials: string;
+export const importCredential: string;
+export const noCredentials: string;
 export const info: string;
 export const explainerLink: string;
