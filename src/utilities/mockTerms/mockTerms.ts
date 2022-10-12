0d0361d5f8d01cdb82fdf3c0e15e99c7fccf949d
Raphael Flechtner <raphael@kilt.io>
2022-10-12 15:42:27 +0200

    chore: update test data

--- src/utilities/mockTerms/mockTerms.ts
+++ src/utilities/mockTerms/mockTerms.ts
@@ -1,24 +1,23 @@
+import type { ICType } from '@kiltprotocol/types';
+import type { Terms } from '../../views/SignQuote/SignQuote';
 import { identitiesMock } from '../identities/IdentitiesProvider.mock';
 
-const mockCType = {
-  hash: '0x240c744923bd98797504328f9bef57ca6c777f5cbf3e1aada74f348c9879d78a',
-  owner: null,
-  schema: {
-    $schema: 'http://kilt-protocol.org/draft-01/ctype#',
-    title: 'BL-Mail-Simple',
-    properties: {
-      'Full name': {
-        type: 'string',
-      },
-      Email: {
-        type: 'string',
-      },
+const mockCType: ICType = {
+  $id: 'kilt:ctype:0x240c744923bd98797504328f9bef57ca6c777f5cbf3e1aada74f348c9879d78a',
+  $schema: 'http://kilt-protocol.org/draft-01/ctype#',
+  title: 'BL-Mail-Simple',
+  properties: {
+    'Full name': {
+      type: 'string',
     },
-    type: 'object',
+    Email: {
+      type: 'string',
+    },
   },
+  type: 'object',
 };
 
-export const mockTerms = {
+export const mockTerms: Terms = {
   claim: {
     cTypeHash:
       '0x240c744923bd98797504328f9bef57ca6c777f5cbf3e1aada74f348c9879d78a',
@@ -27,12 +26,12 @@ export const mockTerms = {
       Email: 'ingo@kilt.io',
     },
     owner:
-      identitiesMock['4tDjyLy2gESkLzvaLnpbn7N61VgnwAhqnTHsPPFAwaZjGwP1'].did,
+      identitiesMock['4tDjyLy2gESkLzvaLnpbn7N61VgnwAhqnTHsPPFAwaZjGwP1'].did!,
   },
   legitimations: [],
   quote: {
     attesterSignature: {
-      keyId:
+      keyUri:
         'did:kilt:4oeJ76hdj84xnwCNqijUHUCTmfwXgSZ4vmxLEiTEYgQdBCcZ#0x59a56e1a4017b60a00c5450bfd6af21dc753e84ef72f2fd45dfc27dd5c3bdafb',
       signature:
         '0x30d01aefd64c7eea48bc743782fe54ba58f96b5a91e33d8d9ee5fae282964967bdea5e9ac3f31756fd251a40ea4d2e9a93ac9b1314cc037cdcb04a53a007e68f',
