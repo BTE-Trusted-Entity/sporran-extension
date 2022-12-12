26651279694148f366a4ba14d633f2151aafe768
Tom Adler <me@arty.name>
2022-12-12 14:27:51 +0100

    increase the stacktrace size in the error message

--- src/components/LoadingSpinner/LoadingSpinner.module.css.d.ts
+++ src/components/LoadingSpinner/LoadingSpinner.module.css.d.ts
@@ -1,3 +1,4 @@
 export const __esModule: true;
 export const spinner: string;
 export const rotation: string;
+export const opacity: string;
--- src/views/GenericError/GenericError.module.css.d.ts
+++ src/views/GenericError/GenericError.module.css.d.ts
@@ -1,3 +1,4 @@
 export const __esModule: true;
+export const overlay: string;
 export const text: string;
 export const details: string;
