48f110121f151f7bc0b04a6dddd64042dccfc9eb
Tom Adler <me@arty.name>
2023-01-23 11:17:27 +0100

    SK-1630: Version 2023.1.23 (#1753)

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -28,7 +28,7 @@ export const internalFeatures: Features = {
 
 // Duplicates the value in src/static/manifest.json
 // We can’t use browser.runtime.getManifest().version, as it’s unavailable in injected scripts
-const version = '2022.12.13';
+const version = '2023.1.23';
 
 export const configuration: ConfigurationType = {
   version,
