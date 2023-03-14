d1f896f8e03d0de1afcace5b3debfe83820726af
lukeg90 <lukegrunau@gmail.com>
2023-03-14 12:29:21 +0100

    Version 2023.3.14

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -28,7 +28,7 @@ export const internalFeatures: Features = {
 
 // Duplicates the value in src/static/manifest.json
 // We can’t use browser.runtime.getManifest().version, as it’s unavailable in injected scripts
-const version = '2023.2.16';
+const version = '2023.3.14';
 
 export const configuration: ConfigurationType = {
   version,
