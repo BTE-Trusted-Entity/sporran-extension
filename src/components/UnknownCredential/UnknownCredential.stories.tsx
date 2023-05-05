017fca9eb088bdc04ae720d83b8658da8a1979a7
Tom Adler <me@arty.name>
2023-05-05 16:16:43 +0200

    added UnknownCredential for SaveCredential

---
+++ src/components/UnknownCredential/UnknownCredential.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta } from '@storybook/react';
+
+import { UnknownCredential } from './UnknownCredential';
+
+export default {
+  title: 'Components/UnknownCredential',
+  component: UnknownCredential,
+} as Meta;
+
+export function Template(): JSX.Element {
+  return (
+    <UnknownCredential rootHash="0xbe7fcc7aa0186b05fd116c100e2d673fb951163693788640ce6032ad2f700dae" />
+  );
+}
