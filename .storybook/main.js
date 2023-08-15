ec6128a7c309947b7cd8e4610288cdf0b8a1376c
Tom Adler <me@arty.name>
2023-08-15 17:32:51 +0200

    storybook cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,18 +9,9 @@ export default {
     '@storybook/addon-viewport',
     '@storybook/addon-controls',
     '@storybook/addon-toolbars',
+    { name: '@storybook/addon-styling', options: { cssModules: { namedExport: true } } },
   ],
-  typescript: {
-    reactDocgen: 'none', // current version doesn’t work with the recent TS
-  },
   webpackFinal: async config => {
-    const cssRule = config.module.rules.find(({
-      test,
-    }) => test.toString() === '/\\.css$/');
-
-    const cssLoaderOptions = cssRule.use[1].options;
-    cssLoaderOptions.modules = { namedExport: true };
-
     config.resolve.alias = {
       ...config.resolve.alias,
       'webextension-polyfill': require.resolve('../src/__mocks__/webextension-polyfill.ts'),
