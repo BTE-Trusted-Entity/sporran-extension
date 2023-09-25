c298913d3e3920f51671592bc89fb0c5627f1c0d
lukeg90 <lukegrunau@gmail.com>
2023-09-25 10:26:55 +0200

    lint

--- src/components/IdentityOptions/IdentityOptions.test.tsx
+++ src/components/IdentityOptions/IdentityOptions.test.tsx
@@ -1,4 +1,4 @@
-import userEvent from '@testing-library/user-event';
+import { userEvent } from '@testing-library/user-event';
 
 import { identitiesMock, render, screen } from '../../testing/testing';
 
--- src/views/ImportBackupPhrase/ImportBackupPhrase.test.tsx
+++ src/views/ImportBackupPhrase/ImportBackupPhrase.test.tsx
@@ -1,4 +1,4 @@
-import userEvent from '@testing-library/user-event';
+import { userEvent } from '@testing-library/user-event';
 import { mnemonicValidate } from '@polkadot/util-crypto';
 import { KiltKeyringPair, Utils } from '@kiltprotocol/sdk-js';
 
