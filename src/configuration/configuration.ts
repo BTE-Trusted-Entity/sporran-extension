306a4754c8cc1bc7d2f27f41ad967307c7d232cd
Adel Golghalyani <48685760+Ad96el@users.noreply.github.com>
2024-02-08 10:39:14 +0100

    v2024.2.7 (#2389)
    * v2024.2.7

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -25,7 +25,7 @@ export const internalFeatures: Features = {
 
 // Duplicates the value in src/static/manifest.json
 // We can’t use browser.runtime.getManifest().version, as it’s unavailable in injected scripts
-const version = '2024.1.17';
+const version = '2024.2.7';
 
 export const configuration: ConfigurationType = {
   version,
