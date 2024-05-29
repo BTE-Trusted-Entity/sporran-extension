d2d82bd47f6b6314e1be464aec54cb18f04288bd
Raphael Flechtner <raphael@kilt.io>
2024-05-29 17:53:09 +0200

    fix: isReadyOrError creates problems

--- src/utilities/initKiltSDK/initKiltSDK.ts
+++ src/utilities/initKiltSDK/initKiltSDK.ts
@@ -6,7 +6,7 @@ import { configuration } from '../../configuration/configuration';
 export async function initKiltSDK(): Promise<void> {
   const address = await getStoredEndpoint();
   if (!ConfigService.isSet('api') || !ConfigService.get('api').isConnected) {
-    await connect(address as any);
+    await connect(address as any).catch(() => ConfigService.get('api').isReady);
   }
 
   if (!configuration.features.finalized) {
