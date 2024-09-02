378a73f816f08da558e725266d96df1bb9b1b64b
kilted-andres <andres@kilt.io>
2024-09-02 16:40:28 +0200

    fix: only use subscan for spiritnet

--- src/utilities/useSubscanHost/useSubscanHost.ts
+++ src/utilities/useSubscanHost/useSubscanHost.ts
@@ -1,15 +1,5 @@
-import { useAsyncValue } from '../useAsyncValue/useAsyncValue';
-import { getEndpoint, KnownEndpoints } from '../endpoints/endpoints';
-
-const subscanHosts: Record<KnownEndpoints, string | undefined> = {
-  'wss://peregrine.kilt.io': 'https://kilt-testnet.subscan.io',
-  'wss://spiritnet.kilt.io': 'https://spiritnet.subscan.io',
-  'wss://kilt.dotters.network': 'https://spiritnet.subscan.io',
-  'wss://kilt.ibp.network': 'https://spiritnet.subscan.io',
-  'wss://peregrine-stg.kilt.io/para': undefined,
-};
+import { isInternal } from '../../configuration/variant';
 
 export function useSubscanHost(): string | undefined {
-  const kiltEndpoint = useAsyncValue(getEndpoint, []);
-  return kiltEndpoint ? subscanHosts[kiltEndpoint] : undefined;
+  return isInternal ? undefined : 'https://spiritnet.subscan.io';
 }
