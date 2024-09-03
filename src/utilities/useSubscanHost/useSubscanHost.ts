380747e3acc5851493efc8fe86a441a95e0255a7
kilted-andres <andres@kilt.io>
2024-09-03 14:39:32 +0200

    feat: whether to use subscan based on chain name fetched from node

--- src/utilities/useSubscanHost/useSubscanHost.ts
+++ src/utilities/useSubscanHost/useSubscanHost.ts
@@ -1,15 +1,10 @@
+import { whichChain } from '../endpoints/whichChain';
 import { useAsyncValue } from '../useAsyncValue/useAsyncValue';
-import { getEndpoint, KnownEndpoints } from '../endpoints/endpoints';
-
-const subscanHosts: Record<KnownEndpoints, string | undefined> = {
-  'wss://peregrine.kilt.io': undefined,
-  'wss://spiritnet.kilt.io': 'https://spiritnet.subscan.io',
-  'wss://kilt.dotters.network': 'https://spiritnet.subscan.io',
-  'wss://kilt.ibp.network': 'https://spiritnet.subscan.io',
-  'wss://peregrine-stg.kilt.io/para': undefined,
-};
 
 export function useSubscanHost(): string | undefined {
-  const kiltEndpoint = useAsyncValue(getEndpoint, []);
-  return kiltEndpoint ? subscanHosts[kiltEndpoint] : undefined;
+  const chainName = useAsyncValue(whichChain, []);
+
+  return chainName === 'KILT Spiritnet'
+    ? 'https://spiritnet.subscan.io'
+    : undefined;
 }
