d321061a43a94ee8693605e9e1763cc7b29d299c
Andrés <105802444+kilted-andres@users.noreply.github.com>
2024-09-03 18:28:39 +0200

    fix: only use subscan for spiritnet (#2644)

---
+++ src/utilities/endpoints/getChainName.ts
@@ -0,0 +1,11 @@
+import { ConfigService } from '@kiltprotocol/sdk-js';
+
+export async function getChainName() {
+  const api = ConfigService.get('api');
+
+  const chainName = (await api.rpc.system.chain()).toHuman();
+
+  console.log('Sporran is connected to a node of the blockchain: ', chainName);
+
+  return chainName;
+}
--- src/utilities/useSubscanHost/useSubscanHost.ts
+++ src/utilities/useSubscanHost/useSubscanHost.ts
@@ -1,15 +1,10 @@
+import { getChainName } from '../endpoints/getChainName';
 import { useAsyncValue } from '../useAsyncValue/useAsyncValue';
-import { getEndpoint, KnownEndpoints } from '../endpoints/endpoints';
-
-const subscanHosts: Record<KnownEndpoints, string | undefined> = {
-  'wss://peregrine.kilt.io': 'https://kilt-testnet.subscan.io',
-  'wss://spiritnet.kilt.io': 'https://spiritnet.subscan.io',
-  'wss://kilt.dotters.network': 'https://spiritnet.subscan.io',
-  'wss://kilt.ibp.network': 'https://spiritnet.subscan.io',
-  'wss://peregrine-stg.kilt.io/para': undefined,
-};
 
 export function useSubscanHost(): string | undefined {
-  const kiltEndpoint = useAsyncValue(getEndpoint, []);
-  return kiltEndpoint ? subscanHosts[kiltEndpoint] : undefined;
+  const chainName = useAsyncValue(getChainName, []);
+
+  return chainName === 'KILT Spiritnet'
+    ? 'https://spiritnet.subscan.io'
+    : undefined;
 }
