f649db9dc12c3cc8f44df0f48cf32698dfaa7d0a
kilted-andres <andres@kilt.io>
2024-09-03 15:25:11 +0200

    fix: don't initialize api again

--- src/utilities/endpoints/whichChain.ts
+++ src/utilities/endpoints/whichChain.ts
@@ -1,10 +1,6 @@
 import { ConfigService } from '@kiltprotocol/sdk-js';
 
-import { initKiltSDK } from '../initKiltSDK/initKiltSDK';
-
 export async function whichChain() {
-  await initKiltSDK();
-
   const api = ConfigService.get('api');
 
   const chainName = (await api.rpc.system.chain()).toHuman();
