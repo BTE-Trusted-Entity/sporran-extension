37891fc39db1e792d8a126a75d02f3cffd8afa79
Andrés <105802444+kilted-andres@users.noreply.github.com>
2024-09-11 14:57:02 +0200

    fix: downgrade manifest.js back to v2 (#2655)
    * Revert "fix: manifest.json (#2632)"
    
    This reverts commit bd79f2a6b036e026647433d52804691a5d1e6c6f.
    
    * chore: set sporran to v2024.9.11

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -25,7 +25,7 @@ export const internalFeatures: Features = {
 
 // Duplicates the value in src/static/manifest.json
 // We can’t use browser.runtime.getManifest().version, as it’s unavailable in injected scripts
-const version = '2024.9.10';
+const version = '2024.9.11';
 
 export const configuration: ConfigurationType = {
   version,
