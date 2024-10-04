af3948309188740f408873ba8554d51ea5a605c7
Andrés <105802444+kilted-andres@users.noreply.github.com>
2024-10-04 17:01:39 +0200

    fix: manifest file for firefox compatibility  (#2678)
    * feat: add-on ID
    
    * fix: use old UUID
    
    * chore: set version to 2024.10.4
    
    * fix: formatting
    
    * feat: both service_worker and scripts on background

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -25,7 +25,7 @@ export const internalFeatures: Features = {
 
 // Duplicates the value in src/static/manifest.json
 // We can’t use browser.runtime.getManifest().version, as it’s unavailable in injected scripts
-const version = '2024.9.23';
+const version = '2024.10.4';
 
 export const configuration: ConfigurationType = {
   version,
